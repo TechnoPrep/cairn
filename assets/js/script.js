c56b0784c8113fb69831940076eb4e8a2c88ad65
nwe <nathan.ehrlich2621@gmail.com>
2021-08-03 21:30:10 -0600

    added functionality to append parks to results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 const apiKey ="hrzeZivocJeVP942upjoq1HS5TL5d1mRUZmDQ64t"
 
+let allParks = $('#parkNames');
 
 function getParkName(){
 
   let queryString = document.location.search;
   let stateCode = queryString.split('=');
-
-  console.log(stateCode);
 
  fetch(`https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&stateCode=&${stateCode}`)
   .then(response => { 
@@ -16,10 +15,68 @@ function getParkName(){
     return response.json();
   })
   .then(data => {
+
     console.log(data.data);
     const html = data.data
      .map(parks => {
-      return `<li>name: ${parks.fullName}</li>`;
+
+      parkEl = $('<li>')
+      parkImgEl = $('<img>');
+      parkNameEl = $('<p>');
+      parkDescEl = $('<p>');
+      parkHoursEl = $('<ul>');
+      parkDailyHoursEl = $('<li>');
+      
+      parkImg = parks.images[0].url;
+      parkImgAlt = parks.images[0].altText;
+      parkName = parks.fullName;
+      parkDesc = parks.description;
+
+      parkHours = parks.operatingHours[0].standardHours;
+
+      const {monday,tuesday,wednesday,thursday,friday,saturday,sunday} = parkHours
+
+      //append data to elements
+
+      parkImgEl.attr('src', parkImg);
+      parkImgEl.attr('alt', parkImgAlt)
+      parkEl.append(parkImgEl);
+
+      parkNameEl.text(parkName);
+      parkEl.append(parkNameEl);
+
+      parkDescEl.text(parkDesc);
+      parkEl.append(parkDescEl);
+      
+      parkDailyHoursEl.text(`Monday: ${monday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Tuesday: ${tuesday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Wednesday: ${wednesday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Thursday: ${thursday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Friday: ${friday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Saturday: ${saturday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      parkDailyHoursEl.text(`Sunday: ${sunday}`);
+      parkEl.append(parkDailyHoursEl);
+
+      
+      allParks.append(parkEl);
+
+      console.log(parkImg);
+      console.log(parkName);
+      console.log(parkDesc);
+      console.log(parkHours);
+
     })
     .join("")
   console.log(html);  
