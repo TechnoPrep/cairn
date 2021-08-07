07fee243f3c8fc69d80d32b0b6bc67b2d77d901d
nwe <nathan.ehrlich2621@gmail.com>
2021-08-07 13:37:04 -0600

    Created function to Store previous states to local storage and skip duplicates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,73 @@
 $('#search').click(function (e) { 
    
     e.preventDefault();
-    let state = $('#state').val();
+    let stateCode = $('#state').val();
+    let stateName = $('#state option:selected').text();
+
+    addToLocal(stateCode, stateName);
 
     if(state !== 'State'){
      
-       $(window).attr('location', `results.html?state=${state}`);
+       $(window).attr('location', `results.html?state=${stateCode}`);
     }
 
-   });
+});
+
+$(document).ready(function () {
+    
+   $(document).on('click', '.custom-state-btn', function(e){
+
+       e.preventDefault();
+       let stateCode = $(this).val();
+   
+       $(window).attr('location', `results.html?state=${stateCode}`);
+   })
+
+});
+
+function addToLocal(stateCode, stateName){
+   let stateArr = [];
+
+   let stateInfo = {
+      Code: stateCode, 
+      Name: stateName
+   }
+
+   stateArr = JSON.parse(localStorage.getItem('previousSelection')) || [];
+
+   stateArr.push(stateInfo);
+
+   // let uniqueState = stateArr.filter((v,i,a)=>a.findIndex(t=>(t.Code === v.Code))===i);
+
+      let uniqueState = stateArr.filter((value,index,array)=>{
+         return array.findIndex(truthy=>{
+            return truthy.Code === value.Code
+         }) === index
+      })
+
+   // let uniqueState = [...new Set(stateArr)];
+
+   localStorage.setItem('previousSelection', JSON.stringify(uniqueState));
+
+}
+
+function displayPrevState(){
+
+   $('#prevState').empty();
+
+   tempArr = JSON.parse(localStorage.getItem('previousSelection'));
+
+   let prevStateEl = $('#prevState');
+   
+   if(tempArr !== null){
+       for (let i = 0; i < tempArr.length; i++) {
+
+         let preStateBtn = `<button value="${tempArr[i].Code}" type="button" class="custom-state-btn">${tempArr[i].Name}</button>`         
+
+         prevStateEl.append(preStateBtn);
+       }
+   }
+
+}
+
+displayPrevState();
